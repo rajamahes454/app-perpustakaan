5e276aff9c45f224f63dc524776a623f243d56f1
rajamahes454 <hesbisnis454@gmail.com>
2026-09-03 09:46:54 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
+
+// Tugas: Route Group dengan prefix /admin
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Selamat datang di panel Admin - Aplikasi Perpustakaan';
+    })->name('admin.info');
 });
